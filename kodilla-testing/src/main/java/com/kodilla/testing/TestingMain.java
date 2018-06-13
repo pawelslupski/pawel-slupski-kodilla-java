fca38714bc878babc5307118003962d9f8e23f98
pawelslupski <pawelslupski.go@gmail.com>
2018-06-13 16:43:23 +0200

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+            SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+            String result = simpleUser.getUsername();
+
+            if (result.equals("theForumUser")) {
+                System.out.println("Test OK");
+            } else {
+                System.out.println("Error!");
+            }
+
+            //Calculator Test
+            Calculator calculator = new Calculator(8, 5);
+
+            int resultAdd = calculator.addAtoB();
+            int resultSubtract = calculator.subtractAFromB();
+
+            System.out.println(calculator.getA());
+            System.out.println(calculator.getB());
+
+            if (resultAdd == 13) {
+                System.out.println("Test OK");
+            } else {
+                System.out.println("Error");
+            }
+
+            if (resultSubtract == 3) {
+                System.out.print("OK again");
+            } else {
+                System.out.println("Error");
+            }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,36 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    private int a;
+    private int b;
+
+    public Calculator(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int addAtoB() {
+        return a + b;
+    }
+
+    public int subtractAFromB() {
+        return a - b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
