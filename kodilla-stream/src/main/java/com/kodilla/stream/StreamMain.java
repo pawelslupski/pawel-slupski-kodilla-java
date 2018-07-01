7551e02ed253dfe06456b58d9f322bef35bdcaa2
pawelslupski <pawelslupski.go@gmail.com>
2018-07-01 12:59:43 +0200

    Enhanced Version of 7.1 & 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,8 +3,6 @@ package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.iterate.NumbersGenerator;
 
-import static jdk.nashorn.internal.objects.NativeString.*;
-
 public class StreamMain {
     public static void main(String[] args) {
         PoemBeautifier poemBeautifier = new PoemBeautifier();
@@ -12,13 +10,11 @@ public class StreamMain {
         poemBeautifier.beautify("Kolejny piekny dzien z Java:P. ", "Udekorujmy ten tekst.",
                 (firstWriting, secondWriting) -> ("ABC " + firstWriting + secondWriting + " ABC"));
         poemBeautifier.beautify("Kolejny piekny dzien z Java:P. ", "Udekorujmy ten tekst.",
-                (firstWriting, secondWriting) -> toUpperCase( firstWriting + secondWriting));
+                (firstWriting, secondWriting) -> ( firstWriting + secondWriting).toUpperCase());
         poemBeautifier.beautify("Kolejny piekny dzien z Java:P. ", "Udekorujmy ten tekst.",
-                (firstWriting, secondWriting) -> toLowerCase( firstWriting + secondWriting));
+                (firstWriting, secondWriting) -> ( firstWriting + secondWriting).toLowerCase());
         poemBeautifier.beautify("Kolejny piekny dzien z Java:P. ", "Udekorujmy ten tekst.",
                 (firstWriting, secondWriting) -> firstWriting.replace("piekny", "brzydki"));
-        poemBeautifier.beautify("Kolejny piekny dzien z Java:P. ", "Udekorujmy ten tekst.",
-                (firstWriting, secondWriting) -> firstWriting.trim());
 
         System.out.println("Using Stream to generate even numbers from 1 to 20");
         NumbersGenerator.generateEven(20);
--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -1,20 +1,19 @@
 package com.kodilla.stream.immutable;
 
-public final class ForumUser {
+public class ForumUser {
     private final String userName;
     private final String realName;
 
+    public ForumUser(final String userName, final String realName) {
+        this.userName = userName;
+        this.realName = realName;
+    }
 
-        public ForumUser(String final userName, String final realName) {
-            this.userName = userName;
-            this.realName = realName;
-        }
+    public String getUserName() {
+        return userName;
+    }
 
-        public String getUserName() {
-            return userName;
-        }
-
-        public String getRealName() {
-            return realName;
-        }
+    public String getRealName() {
+        return realName;
+    }
 }
