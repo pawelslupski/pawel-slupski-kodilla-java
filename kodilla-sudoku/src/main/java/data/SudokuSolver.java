fe199d5919b5a16e3363177a86bb934280fadb1a
pawelslupski <40068798+pawelslupski@users.noreply.github.com>
2018-11-11 12:17:13 +0100

    Update SudokuSolver.java

--- kodilla-sudoku/src/main/java/data/SudokuSolver.java
+++ kodilla-sudoku/src/main/java/data/SudokuSolver.java
@@ -12,7 +12,6 @@ public class SudokuSolver {
         this.board = board;
     }
 
-    // we check if a possible number is already in a row
     private boolean checkTheRow(int row, int number) {
         for (int i = 0; i < SIZE; i++)
             if (board.getSudokuRows().get(row).getElements().get(i).getValue() == number)
@@ -21,7 +20,6 @@ public class SudokuSolver {
         return false;
     }
 
-    // we check if a possible number is already in a column
     private boolean checkTheCol(int col, int number) {
         for (int i = 0; i < SIZE; i++)
             if (board.getSudokuRows().get(i).getElements().get(col).getValue() == number)
@@ -30,7 +28,6 @@ public class SudokuSolver {
         return false;
     }
 
-    // we check if a possible number is in its 3x3 box
     private boolean checkTheBox(int row, int col, int number) {
         int startRow = row - row % BOX_SIZE;
         int startCol = col - col % BOX_SIZE;
@@ -42,36 +39,30 @@ public class SudokuSolver {
         return false;
     }
 
-    // combined method to check if a number possible to a row,col position is ok
     private boolean canSetValue(int row, int col, int number) {
         return !checkTheRow(row, number) && !checkTheCol(col, number) && !checkTheBox(row, col, number);
     }
 
-    // Solve method. We will use a recursive BackTracking algorithm.
-    // we will see better approaches in next video :)
     public boolean solve() {
         for (int row = 0; row < SIZE; row++) {
             for (int col = 0; col < SIZE; col++) {
-                // we search an empty cell
                 if (board.getSudokuRows().get(row).getElements().get(col).getValue() == EMPTY) {
-                    // we try possible numbers
                     for (int number = MIN_VALUE; number <= MAX_VALUE; number++) {
                         if (canSetValue(row, col, number)) {
-                            // number ok. it respects sudoku constraints
                             board.getSudokuRows().get(row).getElements().get(col).setValue(number);
 
-                            if (solve()) { // we start backtracking recursively
+                            if (solve()) {
                                 return true;
-                            } else { // if not a solution, we empty the cell and we continue
+                            } else {
                                 board.getSudokuRows().get(row).getElements().get(col).setValue(EMPTY);
                             }
                         }
                     }
-                    return false; // we return false
+                    return false;
                 }
             }
         }
-        return true; // sudoku solved
+        return true;
     }
 
 }
