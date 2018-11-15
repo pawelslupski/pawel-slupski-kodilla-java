5693ce43ddb290b4252e0ca5e443d94cae628815
pawelslupski <pawelslupski.go@gmail.com>
2018-11-15 12:34:58 +0100

    projectUpdate

--- kodilla-sudoku/src/main/java/app/GameApp.java
+++
@@ -1,8 +0,0 @@
-package app;
-
-public class GameApp {
-    public static void main(String[] args) {
-        GameController gameController = new GameController();
-        gameController.controlLoop();
-    }
-}
--- kodilla-sudoku/src/main/java/data/Prototype.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-public class Prototype<T> implements Cloneable {
-    @Override
-    public T clone() throws CloneNotSupportedException {
-        return (T)super.clone();
-    }
-
-}
