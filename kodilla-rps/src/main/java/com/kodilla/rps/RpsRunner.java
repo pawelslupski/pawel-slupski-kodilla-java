b9f2ef7def9a828e0d5e1069b4e196fcfa5af82b
pawelslupski <pawelslupski.go@gmail.com>
2018-11-26 20:02:07 +0100

    conditional breakpoint example

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.rps;
-
-public class RpsRunner {
-    public static void main(String[] args) {
-        RockPaperScissors rockPaperScissors = new RockPaperScissors();
-        rockPaperScissors.entryToTheGame();
-        rockPaperScissors.playTheGame();
-    }
-}
